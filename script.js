fe0016d2a5d2124049a4ff69ef006d2f9c9178d6
Shubhee01 <77564393+Shubhee01@users.noreply.github.com>
2021-02-20 13:39:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+// The idea of this assignment is to take an existing array of names
+// and then output either Hello 'Name' or Good Bye 'Name' to the console.
+// The program should say "Hello" to any name except names that start with a "J"
+// or "j", otherwise, the program should say "Good Bye". So, the final output
+// on the console should look like this:
+/*
+Hello Yaakov
+Good Bye John
+Good Bye Jen
+Good Bye Jason
+Hello Paul
+Hello Frank
+Hello Larry
+Hello Paula
+Hello Laura 
+Good Bye Jim */
+var names =["Yakoov","John","Jen","Jason","Paul","Frank","Larry","Paula","Laura","Jim"];
+function SayHello(names){
+    console.log("Hello " + names);
+}
+
+function SayBye(names){
+    console.log("Good Bye " + names);
+}
+for(var i=0;i<names.length;i++){
+    var res = names[i].charAt(0);
+    if(res==='J'){
+        SayBye(names[i]);
+    }
+    else{
+        SayHello(names[i]);
+    }
+}
